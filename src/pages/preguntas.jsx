a6640bda0cc1b75e2b2b0e948a6ccce8f5732e76
francolind <90268531+francolind@users.noreply.github.com>
2022-11-28 10:58:20 -0300

    arreglando pagina preguntas2

--- src/pages/preguntas.jsx
+++ src/pages/preguntas.jsx
@@ -1,17 +1,12 @@
 
 
-export const Preguntas = ()=>{
+export const Preguntas =()=>{
     return(
         <div className="miPerfil">
              <div>
-                
                 <h2>¿quieres preguntarnos algo?</h2>
                 <h2>preguntas frecuentes</h2>
-                
              </div>
-           
-           
-            
         </div>
     )
 }
