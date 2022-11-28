6c43df9cbbe66379a4031c47f15c9394dbdd7f91
francolind <90268531+francolind@users.noreply.github.com>
2022-11-28 11:24:05 -0300

    m pagina resumen

---
+++ src/pages/Resumen.jsx
@@ -0,0 +1,14 @@
+export const Resumen = () => {
+    return(
+        <div>
+             <div>  
+                <h2>compras </h2>
+                <h2>historial de compras</h2>
+                <h2>favoritos</h2>
+                <h2>comentarios</h2>
+                <h2>puntuaciones a productos</h2>
+                <h1>factura</h1>
+             </div>
+        </div>
+    )
+}
